76bd8b91eb2e3b121d42bd9e5a2da093107d76b6
David Pearson <David@DavidPearson.biz>
2020-04-29 17:58:01 -0700

    corrected package names

--- mylibrarytwo/src/androidTest/java/biz/davidpearson/mylibrarytwo/ExampleInstrumentedTest.kt
+++ mylibrarytwo/src/androidTest/java/biz/davidpearson/mylibrarytwo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package biz.davidpearson.mylibraryone
+package biz.davidpearson.mylibrarytwo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
